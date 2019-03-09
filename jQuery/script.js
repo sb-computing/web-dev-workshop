a05cc365d3b6831037e889861739384c87ccfbe0
Yang Yang <yangyang1998@gmail.com>
2019-03-08 22:05:07 -0500

    Initial commit

---
+++ jQuery/script.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+	$('#btnSubmit').click(function() {
+		$('.h3').toggle();
+	});
+});
---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+let x = 2 + 3;
+console.log(x);
+
+/*
+function btnClick() {
+	alert('Clicked!');
+}
+*/
+
+let button = document.getElementById("btnSubmit");
+button.onclick = function() { alert('Clicked Again!'); };
+
